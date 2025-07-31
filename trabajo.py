e4667c2ac59841cce6670d04238170d4e1da6b44
SantiagoPastranaB <117896312+SantiagoPastranaB@users.noreply.github.com>
2025-07-31 21:49:51 +0000

    auxilio

--- trabajo.py
+++ trabajo.py
@@ -1,11 +1,16 @@
 
 boo= True
 arr=[0,1]
+arr2=[]
+acu=0
 
 while(boo):
     
     print("Presione 1 para imprimir n terminos de la sucesiòn")
     print("Presione 2 para imprimir el n-èsimo termino de la sucesiòn")
+    print("Presione 3 para sumar el array")
+    print("Presione 4 para salir")
+
     z=int(input("ELija una opciòn: "))
 
     if z==1:
@@ -28,6 +33,21 @@ while(boo):
             b=c
             arr.append(c)
         print(arr[i-1])
+    elif z==3:
+        p=int(input("Ingrese el tamaño del array: "))
 
+        while p>0:
+            g=int(input("Ingrese un valor: "))
+            arr2.append(g)
+            p=p-1
+        for i in arr2:
+            acu=acu+i
+        print(acu)
+    elif z==4:
+        print("Saliendo...")
+        boo=False
+    else:
+        print("Error de digitaciòn")           
 
+            
         
